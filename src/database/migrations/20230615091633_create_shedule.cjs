4401ba631ec1961900884e4da546db436273c4bd
Evgenij <jeniaryabko@bk.ru>
2023-07-07 12:22:22 +0300

    phone

--- src/database/migrations/20230615091633_create_shedule.cjs
+++ src/database/migrations/20230615091633_create_shedule.cjs
@@ -6,7 +6,6 @@ exports.up = function (knex) {
     t.string('phone', 20).defaultTo('Не указано').comment('Телефон больного');
     t.boolean('is_phone').defaultTo(false).comment('Дозвонились ли');
     t.boolean('is_comming').defaultTo(false).comment('Пришел ли');
-    t.string('phone', 20).defaultTo('Не указано').comment('Телефон больного');
     t.string('description', 500).comment('Описание');
     t.integer('doctor_id')
       .references('users.id')
